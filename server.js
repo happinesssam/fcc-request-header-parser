04c1469aff0e119551e0ea25e886714522ae1eaf
happinesssam <sam.bellman@gmail.com>
2016-09-09 16:20:44 +0000

    now your ip

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     var agent = useragent.parse(req.headers['user-agent']);
-    var ip = require('ip');
     res.render('index', { language:req.get('accept-language').split(",")[0],
-    ipaddress: ip.address(), 
+    ipaddress: req.get("x-forwarded-for"), 
     software:agent.toString()});
 }); 
 
